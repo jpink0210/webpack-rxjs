8565098455d298d517afc8c6e231338a388ff3d6
焦品貴 <jiaopingui@jiaopinguide-MacBook-Pro.local>
2019-02-12 19:31:24 +0800

    add express

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 8008;
 app.listen(port);
 console.log('server started '+ port);
